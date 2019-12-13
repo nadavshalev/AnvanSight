3e6e016eef9b8e8dbf89f1a90b40aed1d60203fd
naama shalev <naamashalev2282@gmail.com>
2019-12-13 18:28:38 +0200

    take pic

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from picamera import PiCamera
 from time import sleep
 
+photo_dir = './photos/'
+photo_name = 'naama.jpg'
 camera = PiCamera()
 
 camera.start_preview()
 sleep(5)
+camera.capture(photo_dir+photo_name)
 camera.stop_preview()
